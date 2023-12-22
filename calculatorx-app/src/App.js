a1027e07be6586d22b1e894eaf88fbdac65d6a32
Saim Khan <93315183+KSAIM09@users.noreply.github.com>
2023-12-22 13:27:46 +0000

    Completed

---
+++ calculatorx-app/src/App.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const App = () => {
+  const [input, setInput] = useState('');
+  const [output, setOutput] = useState('');
+  
+
+    const handleButtonClick = (value) => {
+      setInput((prevInput) => prevInput + value);
+    };
+
+  
+    const handleCalculate = () => {
+      try {
+       
+        if (input.trim() === '=' || input.trim() === '') {
+          setOutput('Error');
+          return;
+        }
+    
+        const result = eval(input);
+        setOutput(result);
+      } catch (error) {
+        setOutput('Error');
+      }
+    };
+
+    const handleClear = () => {
+      setInput('');
+      setOutput('');
+    };
+
+    // const buttons = [7, 8, 9, '+', 4, 5, 6, '-', 1, 2, 3, '*', 'C', 0, '=', '/'];
+
+    return (
+        <div className="calculator">
+          <h1>React Calculator</h1>
+            <input className="display" value={input} readOnly />
+            <div className="output">{output}</div>
+
+
+            <div className="buttons">
+                {[7, 8, 9, '+', 4, 5, 6, '-', 1, 2, 3, '*', 'C', 0, '=', '/'].map((button, index) => (
+                    <button key={index} onClick={() => {
+                      if (button === '=')
+                        handleCalculate();
+                      else if (button === 'C')
+                        handleClear();
+                      else
+                        handleButtonClick(button);
+                    }}>
+                      {button}
+                    </button>
+                ))}
+            </div>
+
+
+        </div>
+    );
+};
+
+export default App;
